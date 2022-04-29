c98387291c3e9c3ea2259d9b45b550f3b296f960
ZydenCF <104456940+ZydenCF@users.noreply.github.com>
2022-04-28 19:47:46 -0500

    Add files via upload

---
+++ ProyectPromedy(David Ccolcca)/David_Ccolcca/Assets/Scripts/MostrarObjecto.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MostrarObjecto : MonoBehaviour
+{
+    public GameObject objeto;
+
+    public void MostrarObjeto()
+    {
+
+        if (objeto.activeSelf == false)
+
+            objeto.SetActive(true);
+        else
+            objeto.SetActive(false);
+    }
+}
